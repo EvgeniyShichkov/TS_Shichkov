3a4ab469c380a5e178317ac1626270fe2f118a66
EvgeniyShichkov <shichkov97@gmail.com>
2024-11-21 18:00:16 +0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginPrettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            prettier: pluginPrettier,
+        },
+        rules: {
+            ...pluginPrettier.configs.recommended.rules,
+            semi: ['error', 'always'],
+            'prefer-const': 'error',
+            'no-unused-vars': 'warn',
+            'prettier/prettier': 'error',
+            'no-undef': 'off',
+            'no-param-reassign': 'off',
+            'arrow-body-style': 'off',
+            'no-shadow': 'off',
+            'no-restricted-syntax': ['off', 'ForOfStatement'],
+        },
+    },
+    configPrettier,
+]
